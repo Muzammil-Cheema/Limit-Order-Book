cc87b6d00c88675ef8ea0835668fd60442a84ea4
Muzammil-Cheema <muzi1747@gmail.com>
2026-06-08 19:44:57 -0400

    feat: add global type aliases and enum classes.

---
+++ include/global.h
@@ -0,0 +1,33 @@
+//
+// Created by Muzammil Cheema on 5/26/26.
+//
+
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+using Id = uint64_t;
+using Share = uint64_t;
+using Price = uint64_t;
+
+enum class ORDER_TYPE_T {
+	MARKET,
+	LIMIT
+};
+
+enum class ORDER_SIDE_T {
+	BUY,
+	SELL
+};
+
+enum class ORDER_STATE_T {
+	INITIAL,
+	OPEN,
+	PARTIAL,
+	FILLED,
+	CANCELLED,
+	REJECTED
+};
+
+
+
+#endif //GLOBAL_H
