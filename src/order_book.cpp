d531107bf9fb307b9214ed70cc9e5bcf2a91b0c7
Muzammil-Cheema <muzi1747@gmail.com>
2026-06-09 11:28:45 -0400

    feat: start implementing placeOrder().

--- src/order_book.cpp
+++ src/order_book.cpp
@@ -2,21 +2,38 @@
 // Created by Muzammil Cheema on 6/2/26.
 //
 
+#include <optional>
+#include <expected>
 #include "order_book.h"
-
-#include <utility>
 
 OrderBook::OrderBook(std::string ticker) : ticker(std::move(ticker)) {}
 
-bool OrderBook::placeOrder(Price price, Share shares, ORDER_TYPE_T type, ORDER_SIDE_T side) {
+std::expected<ORDER_STATE_T, PLACE_ORDER_ERROR_CODE> placeOrder(const ORDER_SIDE_T side, const ORDER_TYPE_T type,
+const Share shares, const std::optional<Price> price) {
+	//Validate that price exists for LIMIT orders only and not for MARKET orders
+	if ( (!price && type == ORDER_TYPE_T::LIMIT) || (price && type == ORDER_TYPE_T::MARKET) ) {
+		return std::unexpected(PLACE_ORDER_ERROR_CODE::INVALID_INPUT);
+	}
+
+	if (type == ORDER_TYPE_T::MARKET) {
+		Order order(side, type, shares);
+
+		//TODO Use iterator to traverse over correct map and price level
+	}
+	else if (type == ORDER_TYPE_T::LIMIT) {
+		Order order(side, type, shares, price.value());
+
+		//TODO Use iterator to traverse over correct map and price level
+	}
+
+	return ORDER_STATE_T::FILLED;
+}
+
+bool OrderBook::cancelOrder(const Id order_id) {
 	return false;
 }
 
-bool OrderBook::cancelOrder(Id order_id) {
-	return false;
-}
-
-const Order &OrderBook::getOrder(const Id order_id) const {
+auto OrderBook::getOrder(const Id order_id) const {
 	if (const auto iter = resting_orders.find(order_id); iter != resting_orders.end()) {
 		return *(iter->second);
 	}
@@ -25,6 +42,6 @@ const Order &OrderBook::getOrder(const Id order_id) const {
 		return iter->second;
 	}
 
-	throw std::invalid_argument("Invalid order id. Order not found.");
+	return std::nullopt;
 }
 
